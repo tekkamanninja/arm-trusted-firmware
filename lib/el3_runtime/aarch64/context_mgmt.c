d3beba854d888ded74255df03ab80404ce5c0d60
Sandrine Bailleux <sandrine.bailleux@arm.com>
2017-04-04 16:48:54 +0100

    Add support to enter S-EL0 from EL3
    This patch adds support to enable entry into S-EL0 from EL3. This enables
    execution of secure firmware services in S-EL0. Flags have been added to the
    header attributes in the entry point information structure that specify
    properties of the S-EL0 execution environment e.g. access to cache maintenance
    instructions and floating point registers. These flags are used at the time of
    context initialisation to correctly populate S-EL1 system registers.
    
    Change-Id: I4ecabbfa3f85e3463f5b5c30f2e34a53d68d3826

--- lib/el3_runtime/aarch64/context_mgmt.c
+++ lib/el3_runtime/aarch64/context_mgmt.c
@@ -134,7 +134,9 @@ static void cm_init_context_common(cpu_context_t *ctx, const entry_point_info_t 
 	 * Initialise SCTLR_EL1 to the reset value corresponding to the target
 	 * execution state setting all fields rather than relying of the hw.
 	 * Some fields have architecturally UNKNOWN reset values and these are
-	 * set to zero.
+	 * set to zero, if the target exception level is privileged. Else assume
+	 * that the caller will take care of setting up the correct execution
+	 * environment with caching and translations enabled at EL0.
 	 *
 	 * SCTLR.EE: Endianness is taken from the entrypoint attributes.
 	 *
@@ -142,6 +144,22 @@ static void cm_init_context_common(cpu_context_t *ctx, const entry_point_info_t 
 	 *  required by PSCI specification)
 	 */
 	sctlr_elx = EP_GET_EE(ep->h.attr) ? SCTLR_EE_BIT : 0;
+
+	if (GET_EL(ep->spsr) == MODE_EL0) {
+		assert(security_state == SECURE);
+		sctlr_elx |= SCTLR_M_BIT | SCTLR_C_BIT | SCTLR_I_BIT;
+
+		/* Allow user-space to perform cache maintenance */
+		if (EP_GET_UCME(ep->h.attr))
+			sctlr_elx |= SCTLR_UCI_BIT | SCTLR_UCT_BIT;
+
+		/* Allow user-space to access FP/SIMD registers */
+		if (EP_GET_UFPE(ep->h.attr))
+			write_ctx_reg(get_sysregs_ctx(ctx),
+				      CTX_CPACR_EL1,
+				      CPACR_EL1_FPEN(CPACR_EL1_FP_TRAP_NONE));
+	}
+
 	if (GET_RW(ep->spsr) == MODE_RW_64)
 		sctlr_elx |= SCTLR_EL1_RES1;
 	else {
